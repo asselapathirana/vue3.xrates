91f866e3ff237ffd54b42aa4beff5b7f99d9fc33
Assela Pathirana <assela@pathirana.net>
2022-11-22 21:12:16 +0100

    base added to vite.config.js - silly typo fixed

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue2.xrates/',
+  base: '/vue3.xrates/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
